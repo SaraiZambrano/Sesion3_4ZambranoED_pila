e602481f49bf777d494d27017291fb89bab522b6
SaraiZambrano <sarai.zambrano@estudiante.uacm.edu.mx>
2024-10-28 15:38:31 -0600

    Confirmacion 1 OK Zambrano
    Datos de desarrollo: Sarai Zambrano

---
+++ Sesion3_4ZambranoED_pilas/src/sesion3_4zambranoed_pilas/Sesion3_4ZambranoED_pilas.java
@@ -0,0 +1,29 @@
+
+package sesion3_4zambranoed_pilas;
+
+import java.util.Stack;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author LISUACM
+ */
+public class Sesion3_4ZambranoED_pilas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Declaracion de pila
+        Stack<Integer> pilaZambrano = new Stack<>();
+        
+       pilaZambrano.push(100);
+       pilaZambrano.push(150);
+       pilaZambrano.push(230);
+       pilaZambrano.push(10);
+       pilaZambrano.push(160);
+       
+        JOptionPane.showMessageDialog(null, "Datos en pila ordenada es :\n"+pilaZambrano);
+    }
+    
+}
